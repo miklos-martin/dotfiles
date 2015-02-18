7d8720a4a6420c7281948e5a551c9539e5dbc28d
Martin Miklós <miklos.martin@gmail.com>
2015-02-18 17:16:16 +0100

    fuzzy git checkout - all refs

--- functions/fuzzy.zsh
+++ functions/fuzzy.zsh
@@ -14,8 +14,7 @@ fgrep() {
 }
 
 fgco() {
-  local branches branch
-  branches=$(git branch) &&
-  branch=$(echo "$branches" | fzf +s +m) &&
+  local branch
+  branch=$(git for-each-ref --format="%(refname:short)" | fzf +s +m) &&
   git checkout $(echo "$branch" | sed "s/.* //")
 }
